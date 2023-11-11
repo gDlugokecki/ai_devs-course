7518dcda501dd8a6ea40fdbe637fa7120510ba21
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2023-11-11 16:36:23 +0100

    C03L03

---
+++ src/exercise/C03L03.ts
@@ -0,0 +1,73 @@
+import * as dotenv from "dotenv";
+
+import { getTaskInput, getTaskToken, submitTaskAnswer } from "../api";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage, SystemMessage } from "langchain/schema";
+import { parseFunctionCall } from "../todoist/helper";
+
+dotenv.config();
+
+const guessWhoSchema = {
+  name: "guessWho",
+  description: "Guess who the person is",
+  parameters: {
+    type: "object",
+    properties: {
+      name: {
+        type: "string",
+        description:
+          "Write briefly and ultra-concise the name and surname person described in text. If you don't know say \"I don't know.\"",
+      },
+      answer: {
+        type: "boolean",
+        description: "you know who it is = true",
+      },
+    },
+    required: ["name", "answer"],
+  },
+};
+
+const guessWho = (name: string, surname: string) => {
+  return name + surname;
+};
+
+const tools: any = {
+  guessWho,
+};
+
+const chat = new ChatOpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+}).bind({
+  functions: [guessWhoSchema],
+});
+
+let hasGuessed = false;
+const hints = [] as string[];
+
+while (!hasGuessed) {
+  const taskData = await getTaskToken("whoami");
+  const task = await getTaskInput<{ hint: string }>(taskData.token);
+
+  hints.push(task.hint);
+
+  const conversation = await chat.invoke([
+    new SystemMessage(
+      `Guess who is described person using the following hints.`
+    ),
+    new HumanMessage(hints.join()),
+  ]);
+  const action = parseFunctionCall(conversation);
+  if (action?.name && tools[action.name]) {
+    console.log(action);
+    if (action.args.answer) {
+      const submitResponse = await submitTaskAnswer(
+        taskData.token,
+        action.args.name
+      );
+      hasGuessed = true;
+      console.log(submitResponse);
+    }
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
