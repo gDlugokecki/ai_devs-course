4e0178cf0ee0c6ddb2fa6b56a82380c3674fed91
Grzegorz Dlugokecki <grzesiek.dlugokecki@gmail.com>
2023-11-11 16:55:21 +0100

    C03L03

--- src/exercise/C03L03.ts
+++ src/exercise/C03L03.ts
@@ -7,6 +7,8 @@ import { parseFunctionCall } from "../todoist/helper";
 
 dotenv.config();
 
+const notSureMessage = "I don't know.";
+
 const guessWhoSchema = {
   name: "guessWho",
   description: "Guess who the person is",
@@ -15,8 +17,7 @@ const guessWhoSchema = {
     properties: {
       name: {
         type: "string",
-        description:
-          "Write briefly and ultra-concise the name and surname person described in text. If you don't know say \"I don't know.\"",
+        description: `Write briefly and ultra-concise the name and surname person described in text. If you don't know say ${notSureMessage}`,
       },
       confident: {
         type: "boolean",
@@ -27,16 +28,9 @@ const guessWhoSchema = {
   },
 };
 
-const guessWho = (name: string, surname: string) => {
-  return name + surname;
-};
-
-const tools: any = {
-  guessWho,
-};
-
 const chat = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.1,
 }).bind({
   functions: [guessWhoSchema],
 });
@@ -57,8 +51,8 @@ while (!hasGuessed) {
     new HumanMessage(hints.join()),
   ]);
   const action = parseFunctionCall(conversation);
-  if (action?.name && tools[action.name]) {
-    if (action.args.confident) {
+  if (action?.name) {
+    if (action.args.confident && action.args.name !== notSureMessage) {
       const submitResponse = await submitTaskAnswer(
         taskData.token,
         action.args.name
@@ -70,3 +64,39 @@ while (!hasGuessed) {
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
 }
+
+// //NO FUNCTION CALLING
+// const chat = new ChatOpenAI({
+//   openAIApiKey: process.env.OPENAI_API_KEY,
+//   temperature: 0.1,
+// });
+
+// const getHint = async () => {
+//   const taskData = await getTaskToken("whoami");
+//   const task = await getTaskInput<{ hint: string }>(taskData.token);
+//   return task.hint;
+// };
+
+// const taskData = await getTaskToken("whoami");
+// const task = await getTaskInput<{ hint: string }>(taskData.token);
+
+// const conversation = [
+//   new SystemMessage(
+//     `I will give you a hint and you will try to guess the person described. Important thing is if you are not 100% sure say "NO" and nothing else. If you're sure then you can return the name of the person.`
+//   ),
+// ];
+
+// let name;
+
+// const guessWho = async (hint: string) => {
+//   conversation.push(new HumanMessage(hint));
+//   const { content } = await chat.call(conversation);
+//   if (content === "NO") {
+//     const hint = await getHint();
+//     await guessWho(hint);
+//   } else {
+//     name = content;
+//   }
+// };
+
+// await guessWho(task.hint);
